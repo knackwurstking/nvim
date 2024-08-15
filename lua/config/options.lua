8ca96bb1109fa427e3f469978ddad80918749cd9
Udo Bauer <knackwurstking.tux@gmail.com>
2024-08-15 20:42:57 +0200

    Set tab width to 4

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,11 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
---vim.o.shiftwidth = 4
---vim.o.tabstop = 4
---vim.o.relativenumber = true
---vim.o.wrap = true
---vim.o.colorcolumn = "80"
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.relativenumber = true
+vim.o.wrap = true
+vim.o.colorcolumn = "80"
 
 vim.o.foldmethod = "expr"
 --vim.o.foldmarker = "{,}"
