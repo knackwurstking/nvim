e43358e84a66f55c6b5d90696643296e0b8642ca
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-18 09:56:01 +0200

    Comment out tab options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
+--vim.o.shiftwidth = 4
+--vim.o.tabstop = 4
 vim.o.relativenumber = true
 vim.o.wrap = true
 vim.o.colorcolumn = "80"
