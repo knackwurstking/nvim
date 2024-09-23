572a1fd5ba6a2ac4466f73dd809d517d88246627
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-23 19:50:33 +0200

    Add markdown preview

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -273,4 +273,9 @@ return {
             })
         end,
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app & yarn install",
+        ft = {"markdown", "md"}
+    },
 }
