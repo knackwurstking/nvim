e719bc96f8096050864afee687a489779406f05d
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-23 07:43:32 +0200

    Add 4 space indent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
---vim.o.shiftwidth = 4
---vim.o.tabstop = 4
 vim.o.relativenumber = true
 vim.o.wrap = true
 vim.o.colorcolumn = "80"
@@ -14,3 +12,8 @@ vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 vim.o.foldenable = true
 
 vim.o.conceallevel = 0
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
