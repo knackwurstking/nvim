5e99932b0b3f199fdeaa9f2aa9c548b6e89f7f0f
Udo Bauer <knackwurstking.tux@gmail.com>
2024-09-23 20:12:57 +0200

    Update markdown

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -275,7 +275,17 @@ return {
     },
     {
         "iamcco/markdown-preview.nvim",
-        build = "cd app & yarn install",
-        ft = {"markdown", "md"}
+        ft = "markdown",
+
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+
+        config = function()
+            vim.g.mkdp_open_ip = "127.0.0.1"
+            vim.g.mkdp_port = 8888
+        end,
+
+        cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
     },
 }
