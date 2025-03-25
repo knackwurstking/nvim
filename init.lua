d99cbe179ac080e35fc15cc21a39137344ae27c6
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-25 08:36:36 +0100

    feat: add lazygit

--- init.lua
+++ init.lua
@@ -989,6 +989,27 @@ require('lazy').setup({
         --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     },
 
+    {
+        'kdheepak/lazygit.nvim',
+        lazy = true,
+        cmd = {
+            'LazyGit',
+            'LazyGitConfig',
+            'LazyGitCurrentFile',
+            'LazyGitFilter',
+            'LazyGitFilterCurrentFile',
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { '<leader>g', '<cmd>LazyGit<cr>', desc = 'Lazy[G]it' },
+        },
+    },
+
     -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
     -- place them in the correct locations.
