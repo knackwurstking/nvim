5d649529584ab3ed0b3e8106766dc0921e67b549
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-03 10:03:15 +0200

    add todo

--- lua/knackwurstking/plugins/bufferline.lua
+++ lua/knackwurstking/plugins/bufferline.lua
@@ -40,6 +40,7 @@ return {
 		vim.keymap.set("n", "<leader>bl", "<cmd> BufferLineCloseLeft <CR>", { desc = "Delete [L]eft" })
 		vim.keymap.set("n", "<leader>br", "<cmd> BufferLineCloseRight <CR>", { desc = "Delete [R]ight" })
 		vim.keymap.set("n", "<leader>bd", "<cmd> bd! <CR>", { desc = "[D]lete current" })
+		-- TODO: Need this <leader>bb keymap from lazyvim again
 
 		for i = 1, 9 do
 			vim.keymap.set("n", "<leader>b" .. i, function()
