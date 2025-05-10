5d89417ff5918ea475ff6ecc7fde1bfb730b88dd
Udo Bauer <knackwurstking.tux@gmail.com>
2025-05-10 03:33:20 +0200

    wip: update & comment out lspconfig setup_handlers part for now, and add fixme

--- lua/knackwurstking/plugins/lsp/lspconfig.lua
+++ lua/knackwurstking/plugins/lsp/lspconfig.lua
@@ -99,78 +99,80 @@ return {
 			},
 		})
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["svelte"] = function()
-				-- configure svelte server
-				lspconfig["svelte"].setup({
-					capabilities = capabilities,
-					on_attach = function(client, bufnr)
-						vim.api.nvim_create_autocmd("BufWritePost", {
-							pattern = { "*.js", "*.ts" },
-							callback = function(ctx)
-								-- Here use ctx.match instead of ctx.file
-								client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-							end,
-						})
-					end,
-				})
-			end,
-			["graphql"] = function()
-				-- configure graphql language server
-				lspconfig["graphql"].setup({
-					capabilities = capabilities,
-					filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-				})
-			end,
-			["emmet_ls"] = function()
-				-- configure emmet language server
-				lspconfig["emmet_ls"].setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
-					},
-				})
-			end,
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				})
-			end,
-			["templ"] = function()
-				lspconfig["templ"].setup({
-					filetypes = { "templ" },
-					settings = {
-						templ = {
-							enable_snippets = true,
-						},
-					},
-				})
-			end,
-		})
+		-- FIXME: attempt to call field 'setup_handlers' (a nil value)
+		--
+		--mason_lspconfig.setup_handlers({
+		--	-- default handler for installed servers
+		--	function(server_name)
+		--		lspconfig[server_name].setup({
+		--			capabilities = capabilities,
+		--		})
+		--	end,
+		--	["svelte"] = function()
+		--		-- configure svelte server
+		--		lspconfig["svelte"].setup({
+		--			capabilities = capabilities,
+		--			on_attach = function(client, bufnr)
+		--				vim.api.nvim_create_autocmd("BufWritePost", {
+		--					pattern = { "*.js", "*.ts" },
+		--					callback = function(ctx)
+		--						-- Here use ctx.match instead of ctx.file
+		--						client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+		--					end,
+		--				})
+		--			end,
+		--		})
+		--	end,
+		--	["graphql"] = function()
+		--		-- configure graphql language server
+		--		lspconfig["graphql"].setup({
+		--			capabilities = capabilities,
+		--			filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+		--		})
+		--	end,
+		--	["emmet_ls"] = function()
+		--		-- configure emmet language server
+		--		lspconfig["emmet_ls"].setup({
+		--			capabilities = capabilities,
+		--			filetypes = {
+		--				"html",
+		--				"typescriptreact",
+		--				"javascriptreact",
+		--				"css",
+		--				"sass",
+		--				"scss",
+		--				"less",
+		--				"svelte",
+		--			},
+		--		})
+		--	end,
+		--	["lua_ls"] = function()
+		--		-- configure lua server (with special settings)
+		--		lspconfig["lua_ls"].setup({
+		--			capabilities = capabilities,
+		--			settings = {
+		--				Lua = {
+		--					-- make the language server recognize "vim" global
+		--					diagnostics = {
+		--						globals = { "vim" },
+		--					},
+		--					completion = {
+		--						callSnippet = "Replace",
+		--					},
+		--				},
+		--			},
+		--		})
+		--	end,
+		--	["templ"] = function()
+		--		lspconfig["templ"].setup({
+		--			filetypes = { "templ" },
+		--			settings = {
+		--				templ = {
+		--					enable_snippets = true,
+		--				},
+		--			},
+		--		})
+		--	end,
+		--})
 	end,
 }
