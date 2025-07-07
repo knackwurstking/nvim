387856d17b04ca532bda0181d581eac68ba9ce7d
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-07 15:44:10 +0200

    Formatting...

--- lua/knackwurstking/plugins/go.lua
+++ lua/knackwurstking/plugins/go.lua
@@ -1,17 +1,22 @@
 return {
 	"ray-x/go.nvim",
+
 	dependencies = { -- optional packages
 		"ray-x/guihua.lua",
 		"neovim/nvim-lspconfig",
 		"nvim-treesitter/nvim-treesitter",
 	},
+
 	opts = {
 		-- lsp_keymaps = false,
 		-- other options
 	},
+
 	config = function(lp, opts)
 		require("go").setup(opts)
+
 		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			pattern = "*.go",
 			callback = function()
@@ -20,7 +25,10 @@ return {
 			group = format_sync_grp,
 		})
 	end,
+
 	event = { "CmdlineEnter" },
+
 	ft = { "go", "gomod" },
+
 	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
