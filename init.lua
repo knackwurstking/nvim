8e6351793c6a184a7145f7e4373b01c54680cdd8
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-08 08:58:02 +0200

    Add treesitter

--- init.lua
+++ init.lua
@@ -57,7 +57,25 @@ require("lazy").setup({
     --     "https://gitprovider.com/exampleuser/myplugin",
     -- },
 
-    { "neovim/nvim-lspconfig" }
+    { "neovim/nvim-lspconfig" },
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        opts = function(_, opts)
+            opts.ignore_install = { 'help' }
+
+            if type(opts.ensure_installed) == 'table' then
+                vim.list_extend(opts.ensure_installed, {
+                    'dockerfile',
+                    'git_config',
+                    'jsdoc',
+                    'make',
+                    'toml',
+                    'vimdoc',
+                })
+            end
+        end,
+    },
 })
 
 -- LSP
