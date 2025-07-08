db90cf992f29327c3ff2b53efb8f140a945bf9b3
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-08 19:44:09 +0200

    Add auto completion

--- init.lua
+++ init.lua
@@ -121,6 +121,43 @@ require("lazy").setup({
             -- log_level = 'debug',
         }
     },
+
+    {
+        'hrsh7th/nvim-cmp',
+        event = "InsertEnter", -- Load on InsertEnter event
+
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'L3MON4D3/LuaSnip',
+            'hrsh7th/cmp-nvim-lua',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+        },
+
+        config = function()
+            local cmp = require('cmp')
+
+            cmp.setup {
+                mapping = cmp.mapping.preset.insert {
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm { select = true },
+                },
+                sources = cmp.config.sources {
+                    { name = 'nvim_lsp' },
+                    { name = 'nvim_lua' },
+                    { name = 'cmp_nvim_lsp_signature_help' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                    { name = 'cmdline' },
+                },
+            }
+        end,
+    },
 })
 
 -- LSP
