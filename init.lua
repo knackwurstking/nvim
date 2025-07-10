cff170ffa7bc8751039656c5423e4f6d9c2d653e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-07-10 09:11:50 +0200

    Add lualine

--- init.lua
+++ init.lua
@@ -145,6 +145,15 @@ require("lazy").setup({
             }
         end,
     },
+
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+        config = function() 
+            require("lualine").setup()
+        end,
+    },
 })
 
 -- LSP
