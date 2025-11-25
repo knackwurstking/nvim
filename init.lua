50dc21a28432b958aa71923194fd8c47276fd69a
Udo Bauer <knackwurstking.tux@gmail.com>
2025-11-25 09:03:53 +0100

    Update ensure installed...

--- init.lua
+++ init.lua
@@ -68,12 +68,12 @@ require("lazy").setup({ -- {{{
         config = function () 
           local configs = require("nvim-treesitter.configs")
 
-          configs.setup({
-              ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-              sync_install = false,
-              highlight = { enable = true },
-              indent = { enable = true },  
-            })
+configs.setup({
+               ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "go", "css" },
+               sync_install = false,
+               highlight = { enable = true },
+               indent = { enable = true },  
+             })
         end
     }, -- }}}
 
