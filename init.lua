79c66a175e9f3f6837914cf7c5e411f1fee67eee
Udo Bauer <knackwurstking.tux@gmail.com>
2025-11-25 08:40:54 +0100

    Auto install (mason) lua lsp

--- init.lua
+++ init.lua
@@ -332,9 +332,11 @@ end,
         end
     }, -- }}}
 
-    {
+{
         "mason-org/mason-lspconfig.nvim",
-        opts = {},
+        opts = {
+            ensure_installed = { "lua_ls" }
+        },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
             "neovim/nvim-lspconfig",
