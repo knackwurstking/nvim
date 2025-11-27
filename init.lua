96196fb824901baff9322fbb9e03e589f5816d6d
Udo Bauer <knackwurstking.tux@gmail.com>
2025-11-27 05:56:24 +0100

    Add keymap to delete all buffers except current
    Added a new keymap <space>bD that deletes all buffers except the current one.
    This provides a convenient way to clean up unused buffers while keeping
    the current buffer open.

--- init.lua
+++ init.lua
@@ -407,6 +407,16 @@ vim.keymap.set("n", "<space>bl", "<cmd> ls <CR>", { desc = "List buffers" })
 vim.keymap.set("n", "<space>bn", "<cmd> bnext <CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<space>bp", "<cmd> bprevious <CR>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<space>bd", "<cmd> bd! <CR>", { desc = "Delete buffer" })
+vim.keymap.set("n", "<space>bD", function()
+	local buffers = vim.api.nvim_list_bufs()
+	local current_buf = vim.api.nvim_get_current_buf()
+
+	for _, buf in ipairs(buffers) do
+		if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) then
+			vim.api.nvim_buf_delete(buf, { force = true })
+		end
+	end
+end, { desc = "Delete all buffers except current" })
 vim.keymap.set("n", "<space>bb", ":e #<CR>", { desc = "Switch to last buffer" })
 vim.keymap.set("n", "<space>bg", function()
 	local buffers = vim.api.nvim_list_bufs()
