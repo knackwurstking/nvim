04ef471a14f43357a6b85aa26c00d51ed3cccdd9
Udo Bauer <knackwurstking.tux@gmail.com>
2025-11-29 05:31:30 +0100

    fix: add goimports formatter for templ files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		formatters_by_ft = {
-			templ = { "templ" },
+			templ = { "templ", "goimports" },
 			go = { "gofmt", "goimports" },
 			html = {},
 		},
