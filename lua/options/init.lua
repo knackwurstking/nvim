6a645dba1aa3488023911ddc87f1d6197edebdef
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-05 10:18:56 +0100

    feat(options): set netrw_bufsettings

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -1,6 +1,6 @@
 require("options.lsp")
 
---vim.g.netrw_bufsettings = 'noma nomod nu nowrap ro nobl'
+vim.g.netrw_bufsettings  = 'noma nomod nu nobl nowrap ro'
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner       = 0
 vim.g.netrw_winsize      = 25
