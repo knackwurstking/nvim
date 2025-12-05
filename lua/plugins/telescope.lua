e9c9fcbcc4ded6e4bbeb9f1fe408c863b82af87b
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-05 10:04:31 +0100

    feat: add telescope configuration (opts)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,16 +2,22 @@ return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
-
 	opts = {
+		manual_mode = true,
+		patterns = { ".git" },
+		detection_methods = { "patterns", "lsp" },
+
 		defaults = {
-			layout_strategy = 'horizontal',
+			layout_strategy = "horizontal",
 			layout_config = {
 				horizontal = {
+					prompt_position = "bottom",
 					width = { padding = 0 },
 					height = { padding = 0 },
+					preview_width = 0.6,
 				},
 			},
+			sorting_strategy = "ascending",
 		},
 	},
 }
