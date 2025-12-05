36446935fd8313c582db29bf1cdb2b054e197d87
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-05 11:42:48 +0100

    refactor: update avante provider model to qwen3-coder:30b

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -29,7 +29,7 @@ return {
 			providers = {
 				openai = {
 					endpoint = "http://192.168.178.52:11434/v1",
-					model = "qwen3-vl:30b",
+					model = "qwen3-coder:30b",
 					api_key = "",
 				},
 			},
