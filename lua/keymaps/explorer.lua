86892a8fe3132f95bb79fe3f10f3230f312218da
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-08 07:02:28 +0100

    feat: changing explorer keymaps

--- lua/keymaps/explorer.lua
+++ lua/keymaps/explorer.lua
@@ -1,4 +1,3 @@
 -- Use <space>e as the leader for explorer commands
-vim.keymap.set('n', '<space>es', ':Sex!<CR>', { desc = "Open file explorer with Sex!" })
-vim.keymap.set('n', '<space>eS', ':Sex<CR>', { desc = "Open file explorer with Sex" })
 vim.keymap.set('n', '<space>ee', ':Explore<CR>', { desc = "Open file explorer with Explore" })
+vim.keymap.set('n', '<space>es', ':Lexplore<CR>', { desc = "Open file explorer with Explore" })
