cac8dc58bb6f5085f80cfe76458672ec3030813b
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-08 07:03:29 +0100

    fix(keymaps): descriptions

--- lua/keymaps/explorer.lua
+++ lua/keymaps/explorer.lua
@@ -1,3 +1,3 @@
 -- Use <space>e as the leader for explorer commands
-vim.keymap.set('n', '<space>ee', ':Explore<CR>', { desc = "Open file explorer with Explore" })
-vim.keymap.set('n', '<space>es', ':Lexplore<CR>', { desc = "Open file explorer with Explore" })
+vim.keymap.set('n', '<space>ee', ':Explore<CR>', { desc = "Open explorer" })
+vim.keymap.set('n', '<space>es', ':Lexplore<CR>', { desc = "Open explorer to the side" })
