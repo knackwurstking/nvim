117bba10a7625fb0f4b48232588d0893ddb67152
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-10 02:44:59 +0100

    Update symbols outline width

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,13 @@
 return {
 	"folke/trouble.nvim",
-	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	opts = {
+		modes = {
+			symbols = {
+				position = "right",
+				width = 40,
+			},
+		}
+	}, -- for default options, refer to the configuration section for custom setup.
 	cmd = "Trouble",
 	keys = {
 		{
