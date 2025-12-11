acdce33641f7dbf37926e6002ae06980d07e4a4b
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-11 19:39:14 +0100

    chore: disable lsp capabilities for gopls, lua_ls, templ, html, ts_ls, cssls
    This change temporarily disables the LSP capabilities configuration for
    multiple language servers (gopls, lua_ls, templ, html, ts_ls, cssls) by
    commenting out the capabilities assignment. This is likely a temporary
    measure while troubleshooting or testing different configurations.
    
    Fixes #issue-number (if applicable)

--- lua/options/lsp.lua
+++ lua/options/lsp.lua
@@ -11,8 +11,6 @@ if not vim.lsp.config['gopls'] then
 	}
 end
 
-local capabilities = require("blink.cmp").get_lsp_capabilities()
-
 local function on_attach(client, bufnr)
 	if vim.tbl_contains(client.server_capabilities.completionProvider.triggerCharacters or {}, '.') then
 		require('cmp').setup.buffer { sources = { { name = 'nvim_lsp' } } }
@@ -29,38 +27,40 @@ local function on_attach(client, bufnr)
 	end
 end
 
+--local capabilities = require("blink.cmp").get_lsp_capabilities()
+
 vim.lsp.config['gopls'] = {
 	filetypes = { "go" },
-	capabilities = capabilities,
+	--capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 vim.lsp.config['lua_ls'] = {
 	filetypes = { "lua" },
-	capabilities = capabilities,
+	--capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 vim.lsp.config['templ'] = {
 	filetypes = { "templ" },
-	capabilities = capabilities,
+	--capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 vim.lsp.config['html'] = {
 	filetypes = { "html" },
-	capabilities = capabilities,
+	--capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 vim.lsp.config['ts_ls'] = {
 	filetypes = { "javascript", "typescript" },
-	capabilities = capabilities,
+	--capabilities = capabilities,
 	on_attach = on_attach,
 }
 
 vim.lsp.config['cssls'] = {
 	filetypes = { "css" },
-	capabilities = capabilities,
+	--capabilities = capabilities,
 	on_attach = on_attach,
 }
