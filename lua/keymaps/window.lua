2697ed21a426cec709ab33bd5d2b5169f9d185c6
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-12 19:19:14 +0100

    feat(keymaps): add vertical and horizontal split keymaps
    Added new keymaps for splitting windows:
    - <leader>ws: vertical split
    - <leader>wv: horizontal split
    
    This enhances window management capabilities within the editor.

--- lua/keymaps/window.lua
+++ lua/keymaps/window.lua
@@ -2,6 +2,11 @@
 
 vim.keymap.set('n', '<leader>ww', '<CMD> :tab split <CR>', { desc = 'Split window into new tab' })
 
+-- Split | VSplit
+
+vim.keymap.set('n', '<leader>ws', '<CMD> :vsplit <CR>', { desc = 'Vertical Split' })
+vim.keymap.set('n', '<leader>wv', '<CMD> :split <CR>', { desc = 'Horizontal Split' })
+
 -- Save / Quit
 
 vim.keymap.set('n', '<leader>q', '<CMD> :wq <CR>', { desc = 'Close' })
