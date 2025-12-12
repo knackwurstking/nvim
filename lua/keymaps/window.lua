24421f3e5476dc87ee74e2abe65cb2ecc119b237
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-12 21:32:06 +0100

    Add some basic tab management/navigation keymaps

--- lua/keymaps/window.lua
+++ lua/keymaps/window.lua
@@ -11,3 +11,9 @@ vim.keymap.set('n', '<leader>wv', '<CMD> :split <CR>', { desc = 'Horizontal Spli
 
 vim.keymap.set('n', '<leader>q', '<CMD> :wq <CR>', { desc = 'Close' })
 vim.keymap.set('n', '<leader>s', '<CMD> :ws <CR>', { desc = 'Save' })
+
+-- Tab Management
+vim.keymap.set('n', '<leader>tt', '<CMD> :tabnew <CR>', { desc = 'New tab' })
+vim.keymap.set('n', '<leader>tc', '<CMD> :tabclose <CR>', { desc = 'Close tab' })
+vim.keymap.set('n', '<leader>tp', '<CMD> :tabprevious <CR>', { desc = 'Previous tab' })
+vim.keymap.set('n', '<leader>tn', '<CMD> :tabnext <CR>', { desc = 'Next tab' })
