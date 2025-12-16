4abe7387160bf6332e37c8d1b0d1a9105376f491
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-16 15:30:26 +0100

    chore: enhance makefile target keymaps with unique shortcuts
    - Improved makefile target keymaps to use unique shortcuts based on target names
    - Added logic to generate unique key combinations when conflicts occur
    - Keymaps now start with <space>m followed by unique identifiers
    - Uses first letters of target words to create initial keymaps
    - Resolves keymap conflicts by appending numbers to make keys unique

--- lua/core/keymaps/make.lua
+++ lua/core/keymaps/make.lua
@@ -1,6 +1,7 @@
 -- Makefile targets keymaps
 -- Dynamically adds keymaps for all makefile commands available at the current project root
 -- Keymaps will start with <space>m, grouped under "Make" in which-key
+-- Uses first letters of target names to create unique keymaps
 
 local M = {}
 
@@ -43,6 +44,37 @@ local function get_make_targets()
 	return targets
 end
 
+-- Function to generate unique keymaps from target names
+local function get_unique_keymaps(targets)
+	local keymaps = {}
+	local used_keys = {}
+
+	for _, target in ipairs(targets) do
+		local key = ""
+		local target_lower = target:lower()
+
+		-- Build key from first letters of words
+		for word in target_lower:gmatch("[%w]+") do
+			key = key .. word:sub(1, 1)
+		end
+
+		-- If key is already taken, we need to make it unique
+		-- Try to add numbers or extend the key with more letters if needed
+		local original_key = key
+		local counter = 1
+		while used_keys[key] do
+			-- If we've already used this key, try to make it unique
+			key = original_key .. counter
+			counter = counter + 1
+		end
+
+		used_keys[key] = true
+		keymaps[target] = key
+	end
+
+	return keymaps
+end
+
 -- Function to add keymaps for make targets
 local function add_make_keymaps()
 	local targets = get_make_targets()
@@ -54,11 +86,14 @@ local function add_make_keymaps()
 			{ "<space>m", group = "Make" },
 		})
 
+		-- Get unique keymaps for targets
+		local keymaps = get_unique_keymaps(targets)
+
 		-- Add keymaps for each target
-		for _, target in ipairs(targets) do
-			-- Use the target name for the keymap
-			local key = "<space>m" .. target
-			vim.keymap.set("n", key, "<cmd>make " .. target .. "<CR>", { desc = "Make: " .. target })
+		for target, key in pairs(keymaps) do
+			-- Use the computed keymap
+			local full_key = "<space>m" .. key
+			vim.keymap.set("n", full_key, "<cmd>make " .. target .. "<CR>", { desc = "Make: " .. target })
 		end
 	end
 end
