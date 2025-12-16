9a4e38f11ecc162ffa527068c66cd99a78a3cce0
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-16 04:16:47 +0100

    chore: update avante.nvim provider model to qwen3-coder:30b

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -2,42 +2,43 @@
 -- Avante AI assistant
 
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  lazy = false,
-  version = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-mini/mini.pick",
-    "nvim-telescope/telescope.nvim",
-    "hrsh7th/nvim-cmp",
-    "ibhagwan/fzf-lua",
-    "stevearc/dressing.nvim",
-    "folke/snacks.nvim",
-    "nvim-tree/nvim-web-devicons",
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
-  config = function()
-    require("avante").setup({
-      windows = {
-        position = "right",
-        width = 45,
-      },
-      provider = "openai",
-      providers = {
-        openai = {
-          endpoint = "http://192.168.178.52:11434/v1",
-          model = "devstral-small-2:latest",
-          api_key = "",
-        },
-      },
-    })
-  end,
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	lazy = false,
+	version = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-mini/mini.pick",
+		"nvim-telescope/telescope.nvim",
+		"hrsh7th/nvim-cmp",
+		"ibhagwan/fzf-lua",
+		"stevearc/dressing.nvim",
+		"folke/snacks.nvim",
+		"nvim-tree/nvim-web-devicons",
+		{
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
+	config = function()
+		require("avante").setup({
+			windows = {
+				position = "right",
+				width = 45,
+			},
+			provider = "openai",
+			providers = {
+				openai = {
+					endpoint = "http://192.168.178.52:11434/v1",
+					--model = "devstral-small-2:latest",
+					model = "qwen3-coder:30b",
+					api_key = "",
+				},
+			},
+		})
+	end,
 }
