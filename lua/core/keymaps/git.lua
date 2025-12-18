ca287f6bd1a64f602397c6af050a77d46c5a274a
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-18 22:32:30 +0100

    feat: add keymap '<space>gc'

--- lua/core/keymaps/git.lua
+++ lua/core/keymaps/git.lua
@@ -4,3 +4,6 @@
 -- Lazygit
 
 vim.keymap.set("n", "<space>gg", "<cmd>LazyGit<CR>", { desc = "Open Lazygit" })
+vim.keymap.set("n", "<space>gc",
+	":AvanteEdit generate a semantic git commit message for the current changes in concise style<CR>",
+	{ desc = "Git commit message generation prompt" })
