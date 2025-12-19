a18679b6e60b8c33b2e6dd41dd72f42365cc172e
Udo Bauer <knackwurstking.tux@gmail.com>
2025-12-19 08:38:48 +0100

    chore: simplify avante git commit message prompt
    - Remove "in concise style" from the AvanteEdit prompt
    - The prompt remains functional while being less restrictive

--- lua/core/keymaps/git.lua
+++ lua/core/keymaps/git.lua
@@ -5,5 +5,5 @@
 
 vim.keymap.set("n", "<space>gg", "<cmd>LazyGit<CR>", { desc = "Open Lazygit" })
 vim.keymap.set("n", "<space>gc",
-	":AvanteEdit generate a semantic git commit message for the current changes in concise style<CR>",
+	":AvanteEdit generate a semantic git commit message for the current changes<CR>",
 	{ desc = "Git commit message generation prompt" })
