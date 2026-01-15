e654f2fedc858a59af523a664764c4d332006c40
Udo Bauer <knackwurstking.tux@gmail.com>
2026-01-15 18:16:22 +0100

    chore(ai): update avante model and request parameters

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -35,13 +35,11 @@ return {
 				openai = {
 					endpoint = "http://192.168.178.53:1234/api/v0",
 					model = "mistralai/devstral-small-2-2512",
-					--model = "qwen3-coder:30b",
 					api_key = "",
 					timeout = 30000,
-					--extra_request_body = {
-					--	temperature = 0.75,
-					--	max_tokens = 120000,
-					--},
+					extra_request_body = {
+						temperature = 0.2,
+					},
 				},
 			},
 		})
