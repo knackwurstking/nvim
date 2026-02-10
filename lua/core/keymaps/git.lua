2cc1c7299478c43f07dac8d6cd0b6a782ce05dfe
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-10 14:49:04 +0100

    chore: simplify avante git commit message prompt

--- lua/core/keymaps/git.lua
+++ lua/core/keymaps/git.lua
@@ -5,5 +5,5 @@
 
 vim.keymap.set("n", "<space>gg", "<cmd>LazyGit<CR>", { desc = "Open Lazygit" })
 vim.keymap.set("n", "<space>gc",
-	":AvanteEdit generate a git commit message for the current changes<CR>",
+	":AvanteEdit generate a git commit message<CR>",
 	{ desc = "Git commit message generation prompt" })
