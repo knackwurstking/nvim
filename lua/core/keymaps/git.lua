d2a73cd34a8b2cc49833b43e2e9de9653b073f1a
Udo Bauer <knackwurstking.tux@gmail.com>
2026-02-10 14:49:57 +0100

    Remove "gc" keymap for generating git commit message

--- lua/core/keymaps/git.lua
+++ lua/core/keymaps/git.lua
@@ -4,6 +4,3 @@
 -- Lazygit
 
 vim.keymap.set("n", "<space>gg", "<cmd>LazyGit<CR>", { desc = "Open Lazygit" })
-vim.keymap.set("n", "<space>gc",
-	":AvanteEdit generate a git commit message<CR>",
-	{ desc = "Git commit message generation prompt" })
