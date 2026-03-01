db4c87392a154fa7e7c9224b7e43627c7ddf05e3
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-01 20:46:15 +0100

    Formatting...

--- lua/plugins/syntax/treesitter.lua
+++ lua/plugins/syntax/treesitter.lua
@@ -5,7 +5,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	tag = "v0.10.0",
 	lazy = false,
-	build = ':TSUpdate',
+	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			highlight = { enable = true },
