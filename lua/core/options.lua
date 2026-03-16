044007da298250669f60171d98ebb5acfafbcfb0
Udo Bauer <knackwurstking.tux@gmail.com>
2026-03-16 20:40:55 +0100

    Disable foldenable

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,53 +2,53 @@
 -- General Neovim settings
 
 -- Netrw settings
-vim.g.netrw_bufsettings  = 'noma nomod nonu nobl nowrap ro rnu'
+vim.g.netrw_bufsettings = "noma nomod nonu nobl nowrap ro rnu"
 vim.g.netrw_browse_split = 0
-vim.g.netrw_banner       = 0
-vim.g.netrw_winsize      = 25
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
 
 -- Wildmenu settings
-vim.g.wildmenu           = true
-vim.g.wildmode           = "longest, list, full"
-vim.g.ignorecase         = true
+vim.g.wildmenu = true
+vim.g.wildmode = "longest, list, full"
+vim.g.ignorecase = true
 
 -- UI settings
-vim.o.showtabline        = 1
-vim.o.undofile           = true
-vim.o.clipboard          = "unnamedplus"
-vim.o.laststatus         = 2
+vim.o.showtabline = 1
+vim.o.undofile = true
+vim.o.clipboard = "unnamedplus"
+vim.o.laststatus = 2
 
 -- Show whitespace characters
-vim.o.list               = true
-vim.o.listchars          = 'tab:→ ,space:·'
+vim.o.list = true
+vim.o.listchars = "tab:→ ,space:·"
 
 -- Indentation and formatting
-vim.o.tabstop            = 4
-vim.opt.shiftwidth       = 4
-vim.opt.expandtab        = false
-vim.opt.mouse            = "a"
+vim.o.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.mouse = "a"
 
 -- Line numbers
-vim.opt.number           = true
-vim.opt.relativenumber   = true
-vim.opt.syntax           = "enable"
-vim.opt.cursorline       = true
-vim.opt.cursorcolumn     = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.syntax = "enable"
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = false
 
 -- Folding
-vim.opt.foldmethod       = "expr"
-vim.opt.foldexpr         = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable        = true
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
 
 -- Spell checking
-vim.opt.spelllang        = { "en_us", "de_de" }
-vim.opt.spell            = true
-vim.opt.colorcolumn      = [[80,120]]
+vim.opt.spelllang = { "en_us", "de_de" }
+vim.opt.spell = true
+vim.opt.colorcolumn = [[80,120]]
 
 -- Scrolling
-vim.opt.scrolloff        = 3
-vim.opt.sidescrolloff    = 3
+vim.opt.scrolloff = 3
+vim.opt.sidescrolloff = 3
 
 -- Search settings
-vim.opt.ignorecase       = true
-vim.opt.smartcase        = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = false
