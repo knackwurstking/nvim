8ac8af190afd653f9b29525d5a5764aca25356c7
Udo Bauer <knackwurstking.tux@gmail.com>
2026-04-14 20:47:47 +0200

    add autocmd to automatically source .session.vim on startup

--- lua/init.lua
+++ lua/init.lua
@@ -9,3 +9,12 @@ require("options")
 require("lsp")
 require("plugins")
 require("keymaps")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argv()[1] == nil and vim.fn.filereadable(".session.vim") == 1 then
+      vim.cmd("silent! source .session.vim")
+    end
+  end,
+  nested = true,
+})
