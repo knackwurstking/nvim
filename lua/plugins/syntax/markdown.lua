f03646c8106979f3de71a999b29508e5199fd961
Udo Bauer <knackwurstking.tux@gmail.com>
2026-04-20 19:43:04 +0200

    Disable browser config for markdown preview (use system default)

--- lua/plugins/syntax/markdown.lua
+++ lua/plugins/syntax/markdown.lua
@@ -6,5 +6,5 @@ vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_close = 1
 vim.g.mkdp_refresh_slow = 0
 vim.g.mkdp_command_for_global = 0
-vim.g.mkdp_browser = "firefox"
-vim.g.mkdp_theme = "gruvbox"
+--vim.g.mkdp_browser = "firefox"
+--vim.g.mkdp_theme = "gruvbox"
