6512a674138606d951e3f3732c7b24c7016cc24b
Udo Bauer <knackwurstking.tux@gmail.com>
2026-04-20 19:37:19 +0200

    Switch to iamcco/markdown-preview.nvim for local image support

--- lua/plugins/syntax/markdown.lua
+++ lua/plugins/syntax/markdown.lua
@@ -1,18 +1,10 @@
--- FIXME: Assets not working with this plugin this way
 vim.pack.add({
-	"https://github.com/selimacerbas/live-server.nvim",
-	"https://github.com/selimacerbas/markdown-preview.nvim",
+	"https://github.com/iamcco/markdown-preview.nvim",
 })
 
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		local ok, mp = pcall(require, "markdown_preview")
-		if ok then
-			mp.setup({
-				instance_mode = "takeover",
-				open_browser = true,
-				debounce_ms = 300,
-			})
-		end
-	end,
-})
+vim.g.mkdp_auto_start = 0
+vim.g.mkdp_auto_close = 1
+vim.g.mkdp_refresh_slow = 0
+vim.g.mkdp_command_for_global = 0
+vim.g.mkdp_browser = "firefox"
+vim.g.mkdp_theme = "gruvbox"
