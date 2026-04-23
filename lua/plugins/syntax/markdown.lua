ccc7fbab06026af169cdf570072907b669e2b589
Udo Bauer <knackwurstking.tux@gmail.com>
2026-04-23 05:35:46 +0200

    Add render-markdown.nvim with treesitter and web-devicons dependencies

--- lua/plugins/syntax/markdown.lua
+++ lua/plugins/syntax/markdown.lua
@@ -8,3 +8,13 @@ vim.g.mkdp_refresh_slow = 0
 vim.g.mkdp_command_for_global = 0
 --vim.g.mkdp_browser = "firefox"
 --vim.g.mkdp_theme = "gruvbox"
+
+vim.pack.add({
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
+    -- 'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
+    -- 'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+})
+
+require('render-markdown').setup({}) -- only mandatory if you want to set custom options
