5e6ba787a006aff53b2827129411b15dfbb5a8b1
Udo Bauer <knackwurstking.tux@gmail.com>
2026-05-21 22:20:53 +0200

    Disable markdown renderer

--- lua/plugins/syntax/markdown.lua
+++ lua/plugins/syntax/markdown.lua
@@ -9,12 +9,14 @@ vim.g.mkdp_command_for_global = 0
 --vim.g.mkdp_browser = "firefox"
 --vim.g.mkdp_theme = "gruvbox"
 
-vim.pack.add({
-    'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
-    -- 'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
-    -- 'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
-    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-})
-
-require('render-markdown').setup({}) -- only mandatory if you want to set custom options
+-- NOTE: Disabled for now
+--
+--vim.pack.add({
+--    'https://github.com/nvim-treesitter/nvim-treesitter',
+--    'https://github.com/nvim-mini/mini.nvim',            -- if you use the mini.nvim suite
+--    -- 'https://github.com/nvim-mini/mini.icons',        -- if you use standalone mini plugins
+--    -- 'https://github.com/nvim-tree/nvim-web-devicons', -- if you prefer nvim-web-devicons
+--    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+--})
+--
+--require('render-markdown').setup({}) -- only mandatory if you want to set custom options
